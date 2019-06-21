c320f30d3bb90e09770a5ffe22b55464735e5829
Byh <1415604657@qq.com>
2019-06-21 18:59:05 +0800

    基本完成页面的大致结构，接下来准备使用ant-design的表达替换对应的结构

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom';
+import './assets/less/index.less';
 import App from './App';
 ReactDOM.render(<Router><App/></Router>,document.getElementById('root'));
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-
+import logo from './logo.png';
+import './index.less';
 export default class Login extends Component {
   render() {
-    return <div>
-      Login
+    return <div className="login">
+      <header className="login-header">
+        <h1><img src={logo} alt="logo图" className="logo"/></h1>
+        <h2 className="title">React项目: 后台管理系统</h2>
+      </header>
+      <section>
+        <form className="login-form">
+          <h3>用户登录</h3>
+          <input type="text" placeholder="用户名"/>
+          <input type="password" placeholder="密码"/>
+          <input type="submit" value="登录" className="login-btn"/>
+        </form>
+      </section>
     </div>;
   }
 }
