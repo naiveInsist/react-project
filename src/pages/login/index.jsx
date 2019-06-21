b02a0759291e9510fee2c3e10207deee08df596c
Byh <1415604657@qq.com>
2019-06-21 19:34:21 +0800

    将对应的input元素替换为antdesign中的表单组件，并调整对应的样式

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import logo from './logo.png';
 import './index.less';
-export default class Login extends Component {
+class Login extends Component {
   render() {
+    const Item = Form.Item;
+    const { getFieldDecorator } = this.props.form;
     return <div className="login">
       <header className="login-header">
         <h1><img src={logo} alt="logo图" className="logo"/></h1>
         <h2 className="title">React项目: 后台管理系统</h2>
       </header>
       <section>
-        <form className="login-form">
+        {/*<form className="login-form">*/}
+        {/*  <h3>用户登录</h3>*/}
+        {/*  <input type="text" placeholder="用户名"/>*/}
+        {/*  <input type="password" placeholder="密码"/>*/}
+        {/*  <input type="submit" value="登录" className="login-btn"/>*/}
+        {/*</form>*/}
+        <Form className="login-form" >
           <h3>用户登录</h3>
-          <input type="text" placeholder="用户名"/>
-          <input type="password" placeholder="密码"/>
-          <input type="submit" value="登录" className="login-btn"/>
-        </form>
+          <Item>
+            {
+              getFieldDecorator("username"
+
+              )(
+                <Input prefix={<Icon type="user"  />}
+                       placeholder="用户名" className="setWidth"/>
+              )
+            }
+          </Item>
+          <Item>
+            {
+              getFieldDecorator("password"
+
+              )(
+                <Input prefix={<Icon type="lock"  />}
+                       placeholder="密码" className="setWidth"/>
+              )
+            }
+          </Item>
+          <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+        </Form>
       </section>
     </div>;
   }
 }
+export default Form.create({ name: 'normal_login' })(Login);
