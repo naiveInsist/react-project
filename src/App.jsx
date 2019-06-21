e048861576c9776b99f06f4eecfa4a74dbb1804c
Byh <1415604657@qq.com>
2019-06-21 18:27:33 +0800

    创建前端登录路由和主页面路由，当输入对应的地址时加载对应的组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import Login from './pages/login';
+import Admin from './pages/admin';
+import { Route,Switch } from 'react-router-dom';
 export default class App extends Component {
   render() {
-    return (<div>
-      <Button type="primary">提交</Button>
-    </div>);
+    return <Switch>
+      <Route path="/login" component={Login}/>
+      <Route path="/" component={Admin}/>
+    </Switch>;
   }
 }
