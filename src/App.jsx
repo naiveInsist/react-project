43e17ea8554a5a26029c5f3a8277086d87d474a3
Byh <1415604657@qq.com>
2019-06-23 11:30:55 +0800

    删除package.json中browserlist中在开发环境的配置，来解决编译时产生的Unknown-browser-query-`android-all的警告，同时将一些不必要的es6类组件修改为工厂函数组件。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Login from './pages/login';
 import Admin from './pages/admin';
 import { Route,Switch } from 'react-router-dom';
-export default class App extends Component {
-  render() {
+export default function App(){
     return <Switch>
       <Route path="/login" component={Login}/>
       <Route path="/" component={Admin}/>
     </Switch>;
-  }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,88 +1,88 @@
-import React, { Component } from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import React from 'react';
+import {Form, Icon, Input, Button} from 'antd';
 import logo from './logo.png';
 import './index.less';
-import { login } from '../../api'
-class Login extends Component {
-  validator = (error,content,callback) => {
-    const name = error.field==='username'?'用户名':'密码'
-    if(!content) {
+import {login} from '../../api'
+
+function Login(props) {
+  const validator = (error, content, callback) => {
+    const name = error.field === 'username' ? '用户名' : '密码'
+    if (!content) {
       callback(`${name}不能为空`);
-    }else if(content.length<4){
+    } else if (content.length < 4) {
       callback(`${name}不能少于四位`);
-    }else if(content.length>13){
+    } else if (content.length > 13) {
       callback(`${name}不能大于13位`);
-    }else if(!/^[a-zA-Z0-9_]+$/.test(content)){
+    } else if (!/^[a-zA-Z0-9_]+$/.test(content)) {
       callback(`${name}输入的字符必须是数组字母下划线`);
-    }else {
+    } else {
       callback();
     }
   }
-  login = (e) => {
+  const loginHandle = (e) => {
     e.preventDefault();
-    this.props.form.validateFields(async (error,msg) => {
-      if(!error) {
-        const { username,password } = msg;
-        const result = await login(username,password);
-        if(result){
-          this.props.history.replace('/');
+    props.form.validateFields(async (error, msg) => {
+      if (!error) {
+        const {username, password} = msg;
+        const result = await login(username, password);
+        if (result) {
+          props.history.replace('/');
         } else {
           // message.error(2);
-          this.props.form.resetFields(['password']);
+          props.form.resetFields(['password']);
         }
-      }else {
+      } else {
         console.log('登录校验失败');
       }
     })
   }
 
-  render() {
-    const Item = Form.Item;
-    const { getFieldDecorator} = this.props.form;
-    return <div className="login">
-      <header className="login-header">
-        <h1><img src={logo} alt="logo图" className="logo"/></h1>
-        <h2 className="title">React项目: 后台管理系统</h2>
-      </header>
-      <section>
-        {/*<form className="login-form">*/}
-        {/*  <h3>用户登录</h3>*/}
-        {/*  <input type="text" placeholder="用户名"/>*/}
-        {/*  <input type="password" placeholder="密码"/>*/}
-        {/*  <input type="submit" value="登录" className="login-btn"/>*/}
-        {/*</form>*/}
-        <Form className="login-form" onSubmit={this.login}>
-          <h3>用户登录</h3>
-          <Item>
-            {
-              getFieldDecorator("username",{
-                rules:[{
-                  validator: this.validator
+  const Item = Form.Item;
+  const {getFieldDecorator} = props.form;
+  return <div className="login">
+    <header className="login-header">
+      <h1><img src={logo} alt="logo图" className="logo"/></h1>
+      <h2 className="title">React项目: 后台管理系统</h2>
+    </header>
+    <section>
+      {/*<form className="login-form">*/}
+      {/*  <h3>用户登录</h3>*/}
+      {/*  <input type="text" placeholder="用户名"/>*/}
+      {/*  <input type="password" placeholder="密码"/>*/}
+      {/*  <input type="submit" value="登录" className="login-btn"/>*/}
+      {/*</form>*/}
+      <Form className="login-form" onSubmit={loginHandle}>
+        <h3>用户登录</h3>
+        <Item>
+          {
+            getFieldDecorator("username", {
+                rules: [{
+                  validator: validator
                 }]
               }
-              )(
-                <Input prefix={<Icon type="user"  />}
-                       placeholder="用户名" className="setWidth"/>
-              )
-            }
-          </Item>
-          <Item>
-            {
-              getFieldDecorator("password",{
-                  rules:[{
-                    validator: this.validator
-                  }]
-                }
-              )(
-                <Input prefix={<Icon type="lock"  />}
-                       placeholder="密码" className="setWidth" type="password"/>
-              )
-            }
-          </Item>
-          <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
-        </Form>
-      </section>
-    </div>;
-  }
+            )(
+              <Input prefix={<Icon type="user"/>}
+                     placeholder="用户名" className="setWidth"/>
+            )
+          }
+        </Item>
+        <Item>
+          {
+            getFieldDecorator("password", {
+                rules: [{
+                  validator: validator
+                }]
+              }
+            )(
+              <Input prefix={<Icon type="lock"/>}
+                     placeholder="密码" className="setWidth" type="password"/>
+            )
+          }
+        </Item>
+        <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+      </Form>
+    </section>
+  </div>;
 }
-export default Form.create({ name: 'normal_login' })(Login);
+
+export default Form.create({name: 'normal_login'})(Login);
